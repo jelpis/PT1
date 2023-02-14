fdf2ab72bdbd976a721862dab6421ea39e375c01
marknoh <marknoh@marknohui-MacBookPro.local>
2023-02-14 15:52:12 +0900

    first commit

---
+++ custom.js
@@ -0,0 +1,16 @@
+// const btns = document.querySelector('.btns li');
+// const boxs = document.querySelector('section article');
+
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListner('click', (e) => {
+//     console.log(i);
+
+//     activation(i, btns);
+//   });
+// }
+// function activation(index, list) {
+//   for (let el of list) {
+//     el.classList.remove('on');
+//   }
+//   list[index].classList.add('on');
+// }
